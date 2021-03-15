437f6ad941e7768b9dd37ee8e53867baae5b08d8
QuintusTheFifth <quentinmeremans1@gmail.com>
2021-03-15 01:58:49 +0100

    up-to-date version

---
+++ src/app/user/login/login.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoinListComponent } from 'src/app/coins/coin-list/coin-list.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CoinListComponent,
+    CommonModule
+  ]
+})
+export class LoginModule { }
